3d8a376fd757c02825fbbd9f1b6673ad589ce442
USER <USER@LAPTOP-KD8AR54D>
2025-06-06 10:50:23 -0500

    Subimos Interfaz Login e Inicio

---
+++ Integrador/src/main/java/com/example/Integrador/IntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Integrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegradorApplication.class, args);
+	}
+
+}
---
+++ Integrador/src/test/java/com/example/Integrador/IntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Integrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
