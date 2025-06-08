1d145573962f8a356e970ec8b761d76088a3a533
yuganshen <120036429+yuganshen@users.noreply.github.com>
2025-06-08 15:27:21 -0500

    Update controller.java

--- Integrador/src/main/java/com/example/Integrador/controller.java
+++ Integrador/src/main/java/com/example/Integrador/controller.java
@@ -1,5 +1,4 @@
-package com.example.demInte;
-
+package com.example.Integrador;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
